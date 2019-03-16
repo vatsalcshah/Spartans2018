f5c88634f7eabd1310e24146867c31aca85bbcc6
Vatsal Shah <vatsalcshah@gmail.com>
2019-03-17 04:38:35 +0530

    Create Company.java

---
+++ app/src/main/java/com/vatsal/spartans/models/Company.java
@@ -0,0 +1,58 @@
+package com.vatsal.spartans.models;
+
+/**
+ * Mock Stock
+ * Created by Yash on 9/15/2018.
+ */
+public class Company {
+    public String id = "z";
+    public String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int rate;
+    public String title;
+
+    Company(){}
+
+    public Company(String image, int rate, String title) {
+        this.image = image;
+        this.rate = rate;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return  "Company ["+id+"] : { " + image + ", " + rate + ", " + title + " }";
+    }
+}
